4ee4b8ee431ed53c004a32f72e27f6387b93d9f2
jrgoncalves85 <jrgoncalves85@gmail.com>
2022-06-02 16:18:57 -0300

    adicionado exercício de PA

---
+++ aulas/ex15.py
@@ -0,0 +1,13 @@
+numeros = [0]
+while True:
+    num = int(input("Entre com um número: "))
+    if num != 0:
+        numeros.append(num)
+        numeros.sort()
+        tamanho = len(numeros)
+        print("Lista atualizada")
+        for i in range (0, tamanho):
+            print(numeros[i])
+    else:
+        break
+print("Programa encerrado")
